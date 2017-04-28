20549aadfe44df32ab0a6ab9abdb43a2852c07e6
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-04-28 19:22:06 +0200

    Lifecycle Method

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+    componentWillMount() {
+        //Open debugger in stimulator: cntr+d
+        console.log('componentWillMount in AlbumList');
+        //debugger;
+    }
+
     render() {
         return (
             <View>
