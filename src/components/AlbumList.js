1d1bb2a4f51c3e2852d06a104d06b822d1ab751b
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-04-28 19:32:37 +0200

    Network Requets

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
     componentWillMount() {
         //Open debugger in stimulator: cntr+d
-        console.log('componentWillMount in AlbumList');
+        //console.log('componentWillMount in AlbumList');
         //debugger;
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response));
     }
 
     render() {
