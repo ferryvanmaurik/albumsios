07a46dc570562694383f89aae38c0b58e7907d20
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-04-28 15:58:08 +0200

    Styling with react native

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
-    return <Text>Albums!</Text>;
+    // Destructuring
+const { textStyle } = styles;
+
+    return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 // Make the component available to other parts of the application
