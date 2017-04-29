a39e13d747a46c48f16c8c56373784344dbcaa41
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-04-29 15:46:59 +0200

    Displaying album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -7,17 +7,24 @@ const AlbumDetail = (props) => {
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={styles.thumbnailContainerStyle}>
                     <Image 
                     style={styles.thumbnailStyle}
                     source={{ uri: props.album.thumbnail_image }} />
                 </View>
                 <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
+                    <Text style={styles.headerTextStyle}>{props.album.title}</Text>
                     <Text>{props.album.artist}</Text>
                 </View>
             </CardSection>
+            <CardSection>
+                <Image 
+                style={styles.imageStyle}
+                source={{ uri: props.album.image}}
+                />
+            </CardSection>
         </Card>
+        
     );
 };
 
@@ -26,9 +33,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
