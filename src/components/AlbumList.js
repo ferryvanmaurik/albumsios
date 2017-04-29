0d3bc0fb2a97d234492d6b0d96bf60046cd3aee8
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-04-29 08:34:01 +0200

    Rendering a lit of albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -12,12 +12,16 @@ class AlbumList extends Component {
         .then(response => this.setState({ albums: response.data }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
     render() {
         console.log(this.state);
 
         return (
             <View>
-                <Text>Album list!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
